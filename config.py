b20cb2afc306f6c592057b6669a904204c697be5
Yugandhar Khair <yugkhair@gmail.com>
2025-04-03 23:39:56 -0700

    more fixes

--- config.py
+++ config.py
@@ -5,7 +5,7 @@ from dotenv import load_dotenv
 load_dotenv()
 
 # News API configuration
-NEWS_API_KEY = os.getenv("NEWS_API_KEY", "397de62e9d9847c2b51f6243e60f3950")
+NEWS_API_KEY = os.getenv("NEWS_API_KEY")
 NEWS_API_URL = "https://newsapi.org/v2/top-headlines"
 DEFAULT_COUNTRY = "us"
 DEFAULT_CATEGORY = "technology"
